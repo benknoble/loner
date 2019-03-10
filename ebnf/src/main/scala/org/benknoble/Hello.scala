a4159745ba72a8d4acf4613f0510e4a79d2edd60
David Ben Knoble <ben.knoble@gmail.com>
2019-03-10 08:25:16 -0400

    Move ebnf files to package org.benknoble

--- ebnf/src/main/scala/org/benknoble/Hello.scala
+++ ebnf/src/main/scala/org/benknoble/Hello.scala
@@ -1,4 +1,4 @@
-package example
+package org.benknoble
 
 object Hello extends Greeting with App {
   println(greeting)
--- ebnf/src/test/scala/org/benknoble/HelloSpec.scala
+++ ebnf/src/test/scala/org/benknoble/HelloSpec.scala
@@ -1,4 +1,4 @@
-package example
+package org.benknoble
 
 import org.scalatest._
 
