d84231f865349bfb3ec5ca5d5d0b61853a39c386
David Ben Knoble <ben.knoble@gmail.com>
2019-03-11 21:22:24 -0700

    Generalize Util.join

--- ebnf/src/main/scala/org/benknoble/ebnf/Util.scala
+++ ebnf/src/main/scala/org/benknoble/ebnf/Util.scala
@@ -1,7 +1,7 @@
 package org.benknoble.ebnf
 
 object Util {
-  def join[A](sep: String, list: List[A]): String = list match {
+  def join[A](sep: String, list: Seq[A]): String = list match {
     case h :: t => h.toString() + t.foldLeft("")(_.toString() + sep + _.toString())
     case Nil => ""
   }
