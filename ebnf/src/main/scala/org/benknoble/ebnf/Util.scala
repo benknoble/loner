6f88d977b29824fdd20d900a9ee145ab194e05a4
David Ben Knoble <ben.knoble@gmail.com>
2019-03-17 23:21:39 -0400

    Eliminate need for Util.join

--- ebnf/src/main/scala/org/benknoble/ebnf/Util.scala
+++
@@ -1,8 +0,0 @@
-package org.benknoble.ebnf
-
-object Util {
-  def join[A](sep: String, list: Seq[A]): String = list match {
-    case h :: t => h.toString() + t.foldLeft("")(_.toString() + sep + _.toString())
-    case Nil => ""
-  }
-}
