420e7d86337933c6296c4ab2f67cc7523671280d
David Ben Knoble <ben.knoble@gmail.com>
2019-04-25 01:09:34 -0400

    Implement Nullable and Starter sets
    The Follower sets are on the way. I just need to write some strange
    magic to convert Sequences to flat structures.
    
    And then we'll be nearly there.
    
    Anticipate a similar issue when you get disjoint Predict sets for
    Alternations.

--- src/main/scala/org/benknoble/Loner.scala
+++ src/main/scala/org/benknoble/Loner.scala
@@ -4,19 +4,95 @@ import org.benknoble.ebnf._
 
 object Loner {
 
-  implicit class Xorable(s: Set[Word]) {
-    def ⊙(t: Set[Word]) =
-      if (s contains ε)
-        (s - ε) union t
+  def ⊙ = SetFilter[Word](ε)
+
+  type NtMap[A] = Map[Nonterminal, A]
+  type Nullable = NtMap[Boolean]
+  type Starter = NtMap[Set[Word]]
+  type Follower = NtMap[Set[Word]]
+
+  def fix[A,B,C](fi: (A, Map[B, C]) => Map[B, C])(f0: Map[B, C]): (A => Map[B, C]) = a => {
+    @annotation.tailrec
+    def go(prev: Map[B, C], cur: Map[B, C]): Map[B, C] =
+      if (prev == cur)
+        cur
       else
-        s
+        go(cur, fi(a, cur))
+
+    go(f0, fi(a, f0))
   }
 
-  def nullable(g: Grammar): Map[Nonterminal, Boolean] = Map()
+  private def mapGrammar[A](f: Expr => A)(g: Grammar): NtMap[A] =
+    g.rules.map { p => p.nt -> f(p.rule) }.toMap
 
-  def starters(g: Grammar): Map[Nonterminal, Set[Word]] = Map()
+  private def nullable_i(g: Grammar, prev: Nullable): Nullable = {
+    def N(e: Expr): Boolean = e match {
+      case `ε` => true
+      case Terminal(_) => false
+      case n: Nonterminal => prev(n)
+      case Sequence(left, right) => N(left) && N(right)
+      case Alternation(left, right) => N(left) || N(right)
+      case Repetition(_) => true
+      case Option(_) => true
+    }
 
-  def followers(g: Grammar): Map[Nonterminal, Set[Word]] = Map()
+    mapGrammar(N)(g)
+  }
+
+  def nullable(g: Grammar): Nullable = {
+    val n0 = g.nonterminals.map(_ -> false).toMap
+    fix(nullable_i)(n0)(g)
+  }
+
+  private def S(base: Starter)(e: Expr): Set[Word] = e match {
+    case `ε` => Set(ε)
+    case t: Terminal => Set(t)
+    case n: Nonterminal => base(n)
+    case Sequence(left, right) => ⊙(S(base)(left))(S(base)(right))
+    case Alternation(left, right) => S(base)(left) union S(base)(right)
+    case Repetition(exp) => S(base)(exp) union Set(ε)
+    case Option(exp) => S(base)(exp) union Set(ε)
+  }
+
+  private def starters_i(g: Grammar, prev: Starter): Starter =
+    mapGrammar(S(prev))(g)
+
+
+  def starters(g: Grammar): Starter = {
+    val nullmap = nullable(g)
+    val s0 = g.nonterminals.map(n => {
+      val value: Set[Word] = if (nullmap(n)) Set(ε) else Set()
+      n -> value
+    }).toMap
+    fix(starters_i)(s0)(g)
+  }
+
+  def followers(g: Grammar): Follower = {
+    val nullmap = nullable(g)
+    val startmap = starters(g)
+    val Sf = S(startmap)(_)
+
+    def F(n: Nonterminal)(e: Expr): Seq[Set[Word]] = e match {
+      // these cases should all be handled by the very specific Seq
+      // implementation
+      // case Sequence(`n`, right) => Seq(Sf(right))
+      // case Sequence(_, Sequence(`n`, right)) => Seq(Sf(right))
+      // case Sequence(Sequence(_, `n`), right) => Seq(Sf(right))
+      case w: Word => Seq(Set())
+      case Sequence(left, right) => F(n)(left) ++ F(n)(right)
+      case Alternation(left, right) => F(n)(left) ++ F(n)(right)
+      case Repetition(exp) => F(n)(exp)
+      case Option(exp) => F(n)(exp)
+    }
+
+    val f0 = g.nonterminals.map(a => {
+      val a_finder = F(a)(_)
+      val f0_a = g.rules.flatMap(p => a_finder(p.rule)).reduce(_ union _)
+      a -> (f0_a - ε)
+    }).toMap
+
+    f0
+  }
 
   def isLLone(g: Grammar): Boolean = false
 
--- src/test/scala/org/benknoble/LonerSpec.scala
+++ src/test/scala/org/benknoble/LonerSpec.scala
@@ -6,6 +6,10 @@ class LonerSpec extends FlatSpec with Matchers {
 
   import org.benknoble.ebnf._
   import ExprImplicits._
+
+  def unpack[A](map: Loner.NtMap[A]) =
+    map.map { case (n, a) => n.name -> a }
+
   val example1 = Grammar(
     'S ::= 'A ~ "$",
     'A ::= 'B ~ 'D ~ 'A || "a",
@@ -14,11 +18,29 @@ class LonerSpec extends FlatSpec with Matchers {
   )
 
   "The Loner object" should "compute nullable sets" in {
-    Loner.nullable(example1) shouldEqual Map(
+    unpack(Loner.nullable(example1)) shouldEqual Map(
       'S -> false,
       'A -> false,
       'B -> true,
       'D -> true
       )
+  }
+
+  it should "compute starter sets" in {
+    unpack(Loner.starters(example1)) shouldEqual Map(
+      'S -> Set[Word]("a", "b", "d"),
+      'A -> Set[Word]("a", "b", "d"),
+      'B -> Set[Word]("b", "d", ε),
+      'D -> Set[Word]("d", ε)
+      )
+  }
+
+  it should "compute follower sets" in {
+    unpack(Loner.followers(example1)) shouldEqual Map(
+      'S -> Set[Word](),
+      'A -> Set[Word]("$"),
+      'B -> Set[Word]("a", "b", "d"),
+      'D -> Set[Word]("a", "b", "d")
+    )
   }
 }
