8c6cc43c4676a91d1399c99e566797d779bca7eb
David Ben Knoble <ben.knoble@gmail.com>
2019-04-28 12:04:11 -0400

    Document loner package

---
+++ src/main/scala/org/benknoble/package.scala
@@ -0,0 +1,13 @@
+package org.benknoble
+
+/** Provides types for computing whether or not an org.benknoble.ebnf.Grammar is
+  * in LL(1)
+  *
+  * ==Overview==
+  * The main object is [[org.benknoble.loner.Loner]]. Given a grammar object
+  * `g`, `Loner(g)` or `Loner.isLLone(g)` is true iff `g` is in LL(1).
+  *
+  * Depends on org.benknoble.ebnf.
+  */
+package object loner{
+}
